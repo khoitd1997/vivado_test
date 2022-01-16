8bbb106bfd140e8209c01eeb74ea6dfbd8159e83
khoitd1997 <khoidinhtrinh@gmail.com>
2022-01-16 13:25:20 -0800

    added base 2021.2

---
+++ ultra96/2021.2/export.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+build_dir="${script_dir}/build"
+rm -rf "${build_dir}"
+mkdir "${build_dir}"
+cd "${build_dir}"
+
+source "/tools/Xilinx/Vivado/2021.2/settings64.sh"
+
+# for interactive do:
+# vivado -mode tcl
+vivado -mode batch -source "${script_dir}/export.tcl"
